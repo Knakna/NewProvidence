c8210a981b782ba2a18a85567de6b75ce1e227e3
knakna <kna@tut.by>
2021-04-26 22:50:45 +0300

    add animation

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-
+// https://swiperjs.com/get-started
 
 const swiper = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
-    
+
+
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
-    
+
+
   });
+
+
+  // https://wowjs.uk/
+
+    new WOW().init();
+
